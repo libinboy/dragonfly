57821303bc7067b610437536d5331c26045bed04
libinboy <903603502@qq.com>
2017-11-22 21:03:06 +0800

    add
    Signed-off-by: libinboy <903603502@qq.com>

---
+++ dragonfly-site/src/main/java/com/dragonfly/boot/site/SiteApplication.java
@@ -0,0 +1,16 @@
+package com.dragonfly.boot.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2017/11/22 0022.
+ */
+@SpringBootApplication
+public class SiteApplication
+{
+    public static void main(String[] args) throws Exception
+    {
+        SpringApplication.run(SiteApplication.class, args);
+    }
+}
